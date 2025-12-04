4708866d026b50247758f0f6a2152ea2c899e4eb
nikosrepos2 <designurlife@outlook.com>
2025-12-04 10:57:14 +0100

    1 - proj basic

---
+++ p.py
@@ -0,0 +1,32 @@
+class chatbook:
+        def   __init__(self):
+                  self.uname = ' '
+                  self.psword= ' '
+                  self.loggedin= False
+                  self.menu()
+
+
+        def menu(self):
+                user_input = input("""welcome to chatbook  how like to proceed"
+                                   1. signup
+                                   2. signin
+                                   3. right a post
+                                   4. message friend
+                                   5. exit""")
+                
+                if user_input == "1":
+                        pass 
+                elif user_input == "2":
+                        pass 
+                elif user_input == "3":
+                        pass 
+                elif user_input == "4":
+                        pass 
+                else:
+                        exit() 
+
+obj = chatbook() 
+
+
+        
+                   
